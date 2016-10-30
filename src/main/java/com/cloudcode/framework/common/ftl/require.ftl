248b0136a22bbb752a41b0506ce27b48a5a53c12
lijian <ljzhuanjiao@gmail.com>
2016-10-30 14:06:20 +0800

    Signed-off-by: lijian <ljzhuanjiao@gmail.com>

--- src/main/java/com/cloudcode/framework/common/ftl/require.ftl
+++ src/main/java/com/cloudcode/framework/common/ftl/require.ftl
@@ -1,8 +1,8 @@
-<script data-main="static/module" src="${request.getContextPath()}/static/require.js"></script>
+<script data-main="static/module" src="/cccommon/static/require.js"></script>
 <script >
 var contextPath="${request.getContextPath()}";
 </script>
-<script  src="${request.getContextPath()}/static/module.js"></script>
+<script  src="/cccommon/static/module.js"></script>
 
 
 
--- src/main/java/com/cloudcode/framework/utils/Sorter.java
+++ src/main/java/com/cloudcode/framework/utils/Sorter.java
@@ -9,12 +9,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.formula.functions.T;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
+//import org.apache.solr.client.solrj.SolrQuery;
+//import org.apache.solr.client.solrj.SolrServer;
+//import org.apache.solr.client.solrj.SolrServerException;
+//import org.apache.solr.client.solrj.response.QueryResponse;
+//import org.apache.solr.common.SolrDocument;
+//import org.apache.solr.common.SolrDocumentList;
 
 public class Sorter implements Serializable{
 
@@ -115,107 +115,107 @@ public class Sorter implements Serializable{
      * @param clzz        对象类型
      * @return
      */
-    public static  void queryHighter(SolrServer server,String solrql,
-            int pageNum,int pageSize,List<String> hlField, String preTag,String postTag,Class<T> clzz,String idName){
-        SolrQuery query = new SolrQuery();
-        query.setQuery(solrql);
-        //设置高亮显示
-        query.setHighlight(true);
-        //添加高亮域
-        for(String hlf : hlField){
-            query.addHighlightField(hlf);
-        }
-        //渲染标签
-        query.setHighlightSimplePre(preTag);
-        query.setHighlightSimplePost(postTag);
-        //分页查询
-        query.setStart((pageNum-1)*pageSize);
-        query.setRows(pageSize);
-        QueryResponse response = null;
-        try {
-            response = server.query(query);
-        } catch (SolrServerException e) {
-            e.printStackTrace();
-            //return null;
-        }
-        
-        //查询到的记录总数
-        long totalRow = Long.valueOf(response.getResults().getNumFound()).intValue();
-        //查询结果集
-        List<T> items = new ArrayList<T>();
-        
-        //查询结果集
-        SolrDocumentList solrDocuments = response.getResults();
-        try {
-            Object obj = null;
-            Method m = null;
-            Class<?> fieldType = null;
-            Map<String,Map<String,List<String>>> highlightMap=response.getHighlighting();
-            for(SolrDocument solrDocument : solrDocuments) {
-                    obj = clzz.newInstance();
-                    Collection<String> fieldNames = solrDocument.getFieldNames();            //得到所有的属性名
-                    for (String fieldName : fieldNames) {
-                        //需要说明的是返回的结果集中的FieldNames()比类属性多
-                        Field[] filedArrays = clzz.getDeclaredFields();                        //获取类中所有属性
-                        for (Field f : filedArrays) {    
-                            //如果实体属性名和查询返回集中的字段名一致,填充对应的set方法
-                            if(f.getName().equals(fieldName)){
-                                
-                                //获取到的属性名
-                                //private java.lang.String com.test.model.Article.id
-                                f = clzz.getDeclaredField(fieldName);    
-                                
-                                //属性类型
-                                //private java.lang.String com.test.model.Article.id
-                                fieldType = f.getType();    
-                                
-                                //构造set方法名  setId
-                                String dynamicSetMethod = dynamicMethodName(f.getName(), "set");
-                                
-                                //获取方法
-                                //public void com.test.model.Article.setId(java.lang.String)
-                                m = clzz.getMethod(dynamicSetMethod, fieldType);
-                                
-                                //获取到的值
-                               // LOG.info(f.getName() + "-->" + dynamicSetMethod+ "=" + fieldType.cast(solrDocument.getFieldValue(fieldName)));
-                                
-                                //获取fieldType类型
-                                fieldType = getFileType(fieldType);
-                                
-                                //获取到的属性
-                                m.invoke(obj, fieldType.cast(solrDocument.getFieldValue(fieldName)));
-                                
-                                for(String hl : hlField){
-                                    if(hl.equals(fieldName)){
-                                        String idv = solrDocument.getFieldValue(idName).toString();
-                                        List<String> hlfList=highlightMap.get(idv).get(fieldName);
-                                        if(null!=hlfList && hlfList.size()>0){
-                                            //高亮添加
-                                            m.invoke(obj, fieldType.cast(hlfList.get(0)));
-                                        }else{
-                                            //正常添加
-                                            m.invoke(obj, fieldType.cast(solrDocument.getFieldValue(fieldName)));
-                                        }
-                                    }
-                                }
-                                
-                                
-                            }
-                            
-                        }
-                        
-                    }
-                    items.add(clzz.cast(obj));
-            }
-            
-        } catch (Exception e) {
-           // LOG.error("highlighter query error." + e.getMessage(), e);
-            e.printStackTrace();
-        }
-
-        //填充page对象
-      //  return new Page<T>(pageNum, pageSize, totalRow, items);
-    }
+//    public static  void queryHighter(SolrServer server,String solrql,
+//            int pageNum,int pageSize,List<String> hlField, String preTag,String postTag,Class<T> clzz,String idName){
+//        SolrQuery query = new SolrQuery();
+//        query.setQuery(solrql);
+//        //设置高亮显示
+//        query.setHighlight(true);
+//        //添加高亮域
+//        for(String hlf : hlField){
+//            query.addHighlightField(hlf);
+//        }
+//        //渲染标签
+//        query.setHighlightSimplePre(preTag);
+//        query.setHighlightSimplePost(postTag);
+//        //分页查询
+//        query.setStart((pageNum-1)*pageSize);
+//        query.setRows(pageSize);
+//        QueryResponse response = null;
+//        try {
+//            response = server.query(query);
+//        } catch (SolrServerException e) {
+//            e.printStackTrace();
+//            //return null;
+//        }
+//        
+//        //查询到的记录总数
+//        long totalRow = Long.valueOf(response.getResults().getNumFound()).intValue();
+//        //查询结果集
+//        List<T> items = new ArrayList<T>();
+//        
+//        //查询结果集
+//        SolrDocumentList solrDocuments = response.getResults();
+//        try {
+//            Object obj = null;
+//            Method m = null;
+//            Class<?> fieldType = null;
+//            Map<String,Map<String,List<String>>> highlightMap=response.getHighlighting();
+//            for(SolrDocument solrDocument : solrDocuments) {
+//                    obj = clzz.newInstance();
+//                    Collection<String> fieldNames = solrDocument.getFieldNames();            //得到所有的属性名
+//                    for (String fieldName : fieldNames) {
+//                        //需要说明的是返回的结果集中的FieldNames()比类属性多
+//                        Field[] filedArrays = clzz.getDeclaredFields();                        //获取类中所有属性
+//                        for (Field f : filedArrays) {    
+//                            //如果实体属性名和查询返回集中的字段名一致,填充对应的set方法
+//                            if(f.getName().equals(fieldName)){
+//                                
+//                                //获取到的属性名
+//                                //private java.lang.String com.test.model.Article.id
+//                                f = clzz.getDeclaredField(fieldName);    
+//                                
+//                                //属性类型
+//                                //private java.lang.String com.test.model.Article.id
+//                                fieldType = f.getType();    
+//                                
+//                                //构造set方法名  setId
+//                                String dynamicSetMethod = dynamicMethodName(f.getName(), "set");
+//                                
+//                                //获取方法
+//                                //public void com.test.model.Article.setId(java.lang.String)
+//                                m = clzz.getMethod(dynamicSetMethod, fieldType);
+//                                
+//                                //获取到的值
+//                               // LOG.info(f.getName() + "-->" + dynamicSetMethod+ "=" + fieldType.cast(solrDocument.getFieldValue(fieldName)));
+//                                
+//                                //获取fieldType类型
+//                                fieldType = getFileType(fieldType);
+//                                
+//                                //获取到的属性
+//                                m.invoke(obj, fieldType.cast(solrDocument.getFieldValue(fieldName)));
+//                                
+//                                for(String hl : hlField){
+//                                    if(hl.equals(fieldName)){
+//                                        String idv = solrDocument.getFieldValue(idName).toString();
+//                                        List<String> hlfList=highlightMap.get(idv).get(fieldName);
+//                                        if(null!=hlfList && hlfList.size()>0){
+//                                            //高亮添加
+//                                            m.invoke(obj, fieldType.cast(hlfList.get(0)));
+//                                        }else{
+//                                            //正常添加
+//                                            m.invoke(obj, fieldType.cast(solrDocument.getFieldValue(fieldName)));
+//                                        }
+//                                    }
+//                                }
+//                                
+//                                
+//                            }
+//                            
+//                        }
+//                        
+//                    }
+//                    items.add(clzz.cast(obj));
+//            }
+//            
+//        } catch (Exception e) {
+//           // LOG.error("highlighter query error." + e.getMessage(), e);
+//            e.printStackTrace();
+//        }
+//
+//        //填充page对象
+//      //  return new Page<T>(pageNum, pageSize, totalRow, items);
+//    }
     
     public static String dynamicMethodName(String name, String dynamic) {
     	String methodName ="";
